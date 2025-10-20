68926e33bd48690749f2e7cd19e3afcaf393ff06
Antoine Ceol <antoine.ceol@gmail.com>
2025-10-20 19:19:03 +0200

    ♻️  Extract settings

--- js/tests/app.test.js
+++ js/tests/app.test.js
@@ -1,6 +1,6 @@
 import { describe, it } from "node:test";
 import assert from "node:assert";
-import { updateTimer } from "../utils.mjs";
+import { updateTimer, updateBackground } from "../utils.mjs";
 
 describe('Update timer', () => {
   [
@@ -14,3 +14,30 @@ describe('Update timer', () => {
     })
   })
 });
+
+describe('updateBackground', () => {
+  const background = {
+    style: { height: 0 }, classList: { value: [] }
+  }
+  const settings = {
+    firstThreshold: 0.3,
+    secondThreshold: 0.6,
+    thirdThreshold: 0.9
+  };
+
+
+  [
+    { progress: 0.2, height: '20px', classe: 'start' },
+    { progress: 0.5, height: '50px', classe: 'critical' },
+    { progress: 0.8, height: '80px', classe: 'very-critical' },
+    { progress: 0.95, height: '95px', classe: 'ending' }
+  ].forEach(({ progress, height, classe }) => {
+    it(`with a progress of ${progress}, it should update the background height to ${height} and pick class '${classe}'`, () => {
+
+      updateBackground({ background, totalHeight: 100, progress, settings });
+      assert.equal(background.style.height, height);
+      assert.equal(background.classList.value.includes(classe), true);
+    });
+  });
+});
+
--- js/utils.mjs
+++ js/utils.mjs
@@ -5,3 +5,17 @@ export function updateTimer(secRemaining) {
   // format into "mm:ss" padded with 0 if needed
   return `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
 }
+
+export function updateBackground({ background, totalHeight, progress, settings }) {
+  const currentHeight = Math.floor(totalHeight * progress);
+  background.style.height = `${currentHeight}px`;
+  background.classList.value = [getClassByProgress(progress, settings)];
+}
+
+function getClassByProgress(p, settings) {
+  // p = percentage between 0 and 1
+  if (p < settings.firstThreshold) return 'start';
+  if (p < settings.secondThreshold) return 'critical';
+  if (p < settings.thirdThreshold) return 'very-critical';
+  return 'ending';
+}
